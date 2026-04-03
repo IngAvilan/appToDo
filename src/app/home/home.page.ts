aa5e1b0cdf23b2e0961c8e31770d8d289a98d671
IngAvilan <jpavilanc@itc.edu.co>
2026-04-02 21:56:21 -0500

    Primer commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ITask } from "./ITask.interface";
+import { AlertController } from "@ionic/angular";
+import { Storage } from "@ionic/storage-angular";
 
 @Component({
   selector: 'app-home',
@@ -8,6 +11,60 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  isToastOpen = false;
+  tasks: ITask[] = [];
 
+  constructor(private _AlertController: AlertController, private _Storage: Storage) { }
+
+  async ngOnInit() {
+    await this._Storage.create();
+
+    const tasks = await this._Storage.get('tasks');
+
+    this.tasks = tasks ? JSON.parse(tasks) : [];
+  }
+
+  async open() {
+    const alert = await this._AlertController.create({
+      header: 'Agrega nueva tarea',
+      inputs: [
+        {
+          type: 'text',
+          name: 'title',
+          placeholder: 'Ingrese tarea'
+        }
+      ],
+      buttons: [{
+        text: 'Cancelar',
+        role: 'cancel'
+      },
+      {
+        text: 'Agregar',
+        handler: (data) => {
+          if (data.title.trim() === '') {
+            this.isToastOpen = true;
+            return;
+          }
+
+          this.tasks.push({
+            id: this.tasks.length + 1,
+            title: data.title,
+            done: false
+          });
+          this.saveTask();
+        }
+      }]
+    });
+
+    await alert.present();
+  }
+  private async saveTask(){
+    await this._Storage.set('tasks', JSON.stringify(this.tasks));
+  }
+  deleteTask(task: ITask){
+    console.log(task);
+  }
+  setOpen(isOpen: boolean) {
+    this.isToastOpen = isOpen;
+  }
 }
